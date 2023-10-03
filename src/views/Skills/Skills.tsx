1ed7ce8295abd4dc23f22c41954b536d4de14cbc
Magui Martin <Magali Martin>
2023-10-02 21:15:19 -0300

    Skills responsive

--- src/views/Skills/Skills.tsx
+++ src/views/Skills/Skills.tsx
@@ -4,28 +4,28 @@ import { SiTailwindcss, SiRedux, SiJavascript, SiExpress, SiPostgresql, SiSequel
 
 const Skills: React.FC = () => {
   return (
-    <div id='skills' className='flex flex-col justify-center ml-[8rem] mr-[8rem] text-white'>
+    <div id='skills' className='flex flex-col justify-center ml-[8rem] mr-[8rem] text-white max-[450px]:ml-[0rem] max-[450px]:mr-[0rem]'>
         <div className='flex justify-center mt-[7rem] pb-[2rem]'>
             <h1 className='font-primary text-4xl gradient-text'>HABILIDADES</h1>
         </div>
-        <div className='flex w-full max-[450px]:flex-col '>
+        <div className='flex w-full max-[450px]:flex-col max-[450px]:items-center '>
             <div className='w-2/4 border-r-[0.08rem] pb-[1.5rem] max-[450px]:border-none'>
                 <div className='text-center p-[2rem] font-primary text-blue-800 text-3xl'>
                     <h1>FRONTEND</h1>
                 </div>
                 <div className='text-2xl flex-col '>
-                    <div className='flex mb-[2rem] justify-evenly max-[450px]:flex-col max-[450px]:items-center'>
-                        <div className='flex items-center w-full justify-center'>
+                    <div className='flex mb-[2rem] justify-evenly max-[450px]:flex-col '>
+                        <div className='flex items-center w-full justify-center max-[450px]:mb-[2rem]'>
                             <SiTailwindcss/>
-                            <p className='ml-[0.5rem]'>Tailwind</p>
+                            <p className='ml-[0.5rem] '>Tailwind</p>
                         </div>
                         <div className='flex items-center w-full justify-center'>
-                            <FaCss3Alt/>
+                            <FaCss3Alt />
                             <p className='ml-[0.5rem]'>CSS</p>                  
                         </div>
                     </div>
                     <div className='flex mb-[2rem] justify-evenly max-[450px]:flex-col '>
-                        <div className='flex items-center w-full justify-center'>
+                        <div className='flex items-center w-full justify-center max-[450px]:mb-[2rem]'>
                             <FaHtml5/>
                             <p className='ml-[0.5rem]'>HTML</p>
                         </div>
@@ -35,7 +35,7 @@ const Skills: React.FC = () => {
                         </div>
                     </div>              
                     <div className='flex justify-evenly max-[450px]:flex-col'>
-                        <div className='flex items-center w-full justify-center'>
+                        <div className='flex items-center w-full justify-center max-[450px]:mb-[2rem]'>
                             <FaReact/>
                             <p className='ml-[0.5rem]'>React</p>
                         </div>
@@ -53,7 +53,7 @@ const Skills: React.FC = () => {
                 </div>
                 <div className=' text-2xl flex-col'>
                     <div className='flex justify-evenly mb-[2rem]'>
-                        <div className='flex items-center w-full justify-center'>
+                        <div className='flex items-center w-full justify-center max-[450px]:mr-[2rem]'>
                             <FaNodeJs/>
                             <p className='ml-[0.5rem]'>NodeJS</p>
                         </div>
@@ -63,7 +63,7 @@ const Skills: React.FC = () => {
                         </div>
                     </div>
                     <div className='flex justify-evenly'>
-                        <div className='flex items-center w-full justify-center'>
+                        <div className='flex items-center w-full justify-center max-[450px]:mr-[2rem]'>
                             <SiPostgresql/>
                             <p className='ml-[0.5rem]'>PostgreSQL</p>
                         </div>
