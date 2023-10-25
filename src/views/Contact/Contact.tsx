f66655d224ec264bda67e65fbdd6f7adf8755592
Magui Martin <Magali Martin>
2023-10-24 23:43:45 -0300

    contacto

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Contact:React.FC = () => {
-    return (
-        
-            <footer>
-                <h1>Contáctame</h1>
-            </footer>
-        
+    return (       
+        <footer id="contact" className='flex flex-col justify-center max-[450px]:mb-[28rem]'>
+            <div className='flex justify-center mt-[7rem]'>
+                <h1 className='font-primary text-4xl gradient-text'>CONTACTAME</h1>
+            </div>
+        </footer>        
     )
 }
 
