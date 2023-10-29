e19bd04906b35f8b5cd581a9a1f9f97ee0acff3b
Magui Martin <magalimartin02@gmail.com>
2023-10-29 15:22:21 -0300

    proyectos

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -32,7 +32,7 @@ const Projects: React.FC  = () => {
               <img src={HenryDogs} alt="HenryDogs" className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 rounded"/>
               <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
               <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                <a className="p-[1rem] border-b border-white text-3xl font-bold text-white" href='' target="_blank">
+                <a className="p-[1rem] border-b border-white text-3xl font-bold text-white" href='https://henry-dogs-front.vercel.app/' target="_blank">
                   Ver más
                 </a>
               </div>
