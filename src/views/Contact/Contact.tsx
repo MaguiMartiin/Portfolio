cd27007add851017e4d5291e20c524bb5406b8e0
Magui Martin <magalimartin02@gmail.com>
2023-11-20 13:08:42 -0300

    responsive

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -54,7 +54,7 @@ return (
         <h1 className='font-primary text-4xl gradient-text'>CONTACTO</h1>
       </div>
 
-      <div className='flex mt-[4rem]'>
+      <div className='flex mt-[4rem] max-[450px]:flex-col'>
         <div className='flex-col flex w-full justify-center items-end mr-[6rem] mb-[4.5rem]'>
           <div className='flex flex-col w-[15rem] h-[10rem] rounded-lg shadow-lg text-center justify-center  bg-slate-400 text-white mb-[2rem]'>
             <div className='flex justify-center'>
@@ -73,7 +73,7 @@ return (
         </div>
 
         <div className='flex-col flex w-full justify-center'>
-          <form onSubmit={handleSubmit} className='flex flex-col justify-center font-primary text-white max-[450px]:mb-[28rem] mr-[8rem]'>
+          <form onSubmit={handleSubmit} className='flex flex-col justify-center font-primary text-white mr-[12rem] max-[450px]:w-full'>
             <div className='p-[1rem] flex flex-col bg-slate-400 mb-[1rem] rounded-xl'>
                 <label htmlFor='name' className='mb-[0.5rem] text-blue-900'>Nombre </label>
                 <input type='text' id='user_name' name='user_name' value={formData.user_name} placeholder='Inserta tu nombre' onChange={handleChange} required className='bg-slate-400 placeholder:text-white font-secondary' />
