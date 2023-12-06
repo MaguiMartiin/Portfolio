949a4479fa0a44c6373569664bde3df43c141ec9
Magui Martin <magalimartin02@gmail.com>
2023-12-06 19:06:35 -0300

    NavBar responsive

--- src/views/NavBar.tsx
+++ src/views/NavBar.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaHome, FaUser, FaTools, FaBriefcase, FaEnvelope } from "react-icons/fa";
 
 const NavBar: React.FC = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isScreenSmall = () => {
+    return windowWidth <= 980;
+  };
 
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId)
@@ -10,36 +27,31 @@ const NavBar: React.FC = () => {
     }
   }
 
+  const navItems = [
+    { icon: <FaHome />, label: "Inicio", sectionId: "home" },
+    { icon: <FaUser />, label: "Sobre mi", sectionId: "about" },
+    { icon: <FaTools />, label: "Habilidades", sectionId: "skills" },
+    { icon: <FaBriefcase />, label: "Proyectos", sectionId: "works" },
+    { icon: <FaEnvelope />, label: "Contacto", sectionId: "contact" },
+  ];
+
   return (
-    <div className="justify-center items-center bg-black text-white p-[0.5rem] fixed w-[50%] rounded-full z-30 shadow-lg shadow-blue-700 mt-[2rem] max-[400px]:w-full max-[450px]:w-full">
-      <div className="flex font-secondary ">
-        <button className="p-[0.5rem] flex items-center w-full justify-center active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700" onClick={() => scrollToSection("home")} >
-          <FaHome className="text-base mr-[0.5rem]" />
-          Inicio
+    <div className={`justify-center items-center bg-black text-white p-[0.5rem] fixed w-[50%] rounded-full z-30 shadow-lg shadow-blue-700 mt-[2rem] max-[400px]:w-full max-[450px]:w-full ${isScreenSmall() ? 'flex' : 'flex'}`}>
+      {navItems.map((item, index) => (
+        <button
+          key={index}
+          className={`p-[0.5rem] flex items-center justify-center ${isScreenSmall() ? 'mb-[0.5rem] w-full' : 'w-full'} active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700`}
+          onClick={() => scrollToSection(item.sectionId)}
+        >
+          {isScreenSmall() ? item.icon : (
+            <>
+              {item.icon}
+              <span className="ml-[0.5rem]">{item.label}</span>
+            </>
+          )}
         </button>
-
-        <button className="p-[0.5rem] flex items-center w-full justify-center active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700" onClick={() => scrollToSection("about")}>
-          <FaUser className="text-base mr-[0.5rem]" />
-          Sobre mi
-        </button>
-
-        <button className="p-[0.5rem] flex items-center w-full justify-center active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700"  onClick={() => scrollToSection("skills")}>
-          <FaTools className="text-base mr-[0.5rem]" />
-          Habilidades
-        </button>
-
-        <button className="p-[0.5rem] flex items-center w-full justify-center active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700"   onClick={() => scrollToSection("works")}>
-          <FaBriefcase className="text-base mr-[0.5rem]" />
-          Proyectos
-        </button>
-
-        <button className="p-[0.5rem] flex items-center w-full justify-center active:border-b-4 active:border-blue-700 focus:border-b-4 focus:border-blue-700"   onClick={() => scrollToSection("contact")}>
-          <FaEnvelope className="ttext-base mr-[0.5rem]" />
-          Contacto
-        </button>
-      </div>
+      ))}
     </div>
-    
   );
 };
 
